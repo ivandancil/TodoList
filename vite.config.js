aa6e130f05efeb308199329f6a11301ba136a0b6
Ivan Dancil <ivanbdancil@gmail.com>
2025-06-01 10:19:16 +0700

    Updated: latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const GITHUB_REPO_NAME = 'TodoList.git';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-list.github.io/",
+  base: `/${GITHUB_REPO_NAME}/`,
 })
